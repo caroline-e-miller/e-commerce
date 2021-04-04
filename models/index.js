c0ee8b1336ffb3f87f211eea4d2c49db7af9caf2
caroline-e-miller <caroline.miller@ymail.com>
2021-04-03 19:29:21 -0500

    updated foreign keys

--- models/index.js
+++ models/index.js
@@ -6,7 +6,7 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: 'product_id',
+  foreignKey: 'category_id',
 });
 
 // Categories have many Products
@@ -16,9 +16,13 @@ Category.hasMany(Product, {
 })
 
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tag, { through: ProductTag });
+Product.belongsToMany(Tag, { through: ProductTag }, {
+  foreignKey: 'tag_id'
+});
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(Product, { through: ProductTag });
+Tag.belongsToMany(Product, { through: ProductTag }, {
+  foreignKey: 'product_id'
+});
 
 module.exports = {
   Product,
